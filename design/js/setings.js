45b7dfbea7c4418cbe257ec1ac31c90ad0808a0b
Ruslan <ssakoks@mail.ru>
2025-02-11 11:27:55 +0500

    add Inter font

--- design/js/setings.js
+++ design/js/setings.js
@@ -1,8 +1,8 @@
 const info = {
-  api: 'http://192.168.0.100/QueueService/json',
+  api: "http://192.168.0.100/QueueService/json",
   // timeBackPage: 30, // сек,
-  firstPage: 'Main',
-  lang: 'ru'
-}
+  firstPage: "Main",
+  lang: "ru",
+};
 
 window.MY_SETINGS_TERMINAL_DESIGN = info;
--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <router-view @updateLoading="loadingActive()"/>
+    <router-view @updateLoading="loadingActive()" />
     <Loading v-show="loading == true" />
   </div>
 </template>
@@ -10,11 +10,11 @@ import Loading from "@/components/Loading.vue";
 export default {
   data() {
     return {
-      loading: false
-    }
+      loading: false,
+    };
   },
   components: {
-    Loading
+    Loading,
   },
   mounted() {
     this.loading = true;
@@ -24,13 +24,13 @@ export default {
       setTimeout(() => {
         this.loading = false;
       }, 5000);
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-*{
+* {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
@@ -53,7 +53,7 @@ p {
   cursor: pointer;
   text-align: center;
   white-space: normal;
-  transition: transform 0.3s
+  transition: transform 0.3s;
   // box-shadow: 0px 5px 10px 10px rgba(211, 211, 211, 0.3) inset;
   // &:active {
   //   box-shadow: 0px 5px 10px 10px rgba(78, 78, 78, 0) inset;
@@ -109,9 +109,9 @@ p {
     &__item {
       width: 56px;
       height: 56px;
-      font-family: 'Roboto';
+      font-family: "Roboto";
       font-size: 2em;
-      text-shadow: 0 1px 1px rgba(255,255,255,0.75);
+      text-shadow: 0 1px 1px rgba(255, 255, 255, 0.75);
       color: #fff;
       background: #484747;
       border: 1px solid #bbb;
@@ -131,123 +131,222 @@ p {
 }
 body {
   -webkit-touch-callout: none; /* iOS Safari */
-  -webkit-user-select: none;   /* Chrome/Safari/Opera */
-  -khtml-user-select: none;    /* Konqueror */
-  -moz-user-select: none;      /* Firefox */
-  -ms-user-select: none;       /* Internet Explorer/Edge */
-  user-select: none;           /* Non-prefixed version, currently
+  -webkit-user-select: none; /* Chrome/Safari/Opera */
+  -khtml-user-select: none; /* Konqueror */
+  -moz-user-select: none; /* Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+  user-select: none; /* Non-prefixed version, currently
                                   not supported by any browser */
   overflow: hidden;
 }
 
 ///////////////////////// Roboto /////////////////////////
 @font-face {
-  font-family: 'Roboto';
-  src: local('Roboto'), url('./assets/fonts/Roboto/Roboto-Thin.ttf') format('truetype');
+  font-family: "Roboto";
+  src: local("Roboto"),
+    url("./assets/fonts/Roboto/Roboto-Thin.ttf") format("truetype");
   font-weight: 100;
 }
 @font-face {
-  font-family: 'Roboto';
-  src: local('Roboto'), url('./assets/fonts/Roboto/Roboto-Light.ttf') format('truetype');
+  font-family: "Roboto";
+  src: local("Roboto"),
+    url("./assets/fonts/Roboto/Roboto-Light.ttf") format("truetype");
   font-weight: 300;
 }
 @font-face {
-  font-family: 'Roboto';
-  src: local('Roboto'), url('./assets/fonts/Roboto/Roboto-Regular.ttf') format('truetype');
+  font-family: "Roboto";
+  src: local("Roboto"),
+    url("./assets/fonts/Roboto/Roboto-Regular.ttf") format("truetype");
   font-weight: 400;
 }
 @font-face {
-  font-family: 'Roboto';
-  src: local('Roboto'), url('./assets/fonts/Roboto/Roboto-Medium.ttf') format('truetype');
+  font-family: "Roboto";
+  src: local("Roboto"),
+    url("./assets/fonts/Roboto/Roboto-Medium.ttf") format("truetype");
   font-weight: 500;
 }
 @font-face {
-  font-family: 'Roboto';
-  src: local('Roboto'), url('./assets/fonts/Roboto/Roboto-Bold.ttf') format('truetype');
+  font-family: "Roboto";
+  src: local("Roboto"),
+    url("./assets/fonts/Roboto/Roboto-Bold.ttf") format("truetype");
   font-weight: 600;
 }
 @font-face {
-  font-family: 'Roboto';
-  src: local('Roboto'), url('./assets/fonts/Roboto/Roboto-Black.ttf') format('truetype');
+  font-family: "Roboto";
+  src: local("Roboto"),
+    url("./assets/fonts/Roboto/Roboto-Black.ttf") format("truetype");
   font-weight: 700;
 }
 
 ///////////////////////// Montserrat /////////////////////////
 @font-face {
-  font-family: 'Montserrat';
-  src: local('Montserrat'), url('./assets/fonts/Montserrat/Montserrat-Thin.ttf') format('truetype');
+  font-family: "Montserrat";
+  src: local("Montserrat"),
+    url("./assets/fonts/Montserrat/Montserrat-Thin.ttf") format("truetype");
   font-weight: 100;
 }
 @font-face {
-  font-family: 'Montserrat';
-  src: local('Montserrat'), url('./assets/fonts/Montserrat/Montserrat-Light.ttf') format('truetype');
+  font-family: "Montserrat";
+  src: local("Montserrat"),
+    url("./assets/fonts/Montserrat/Montserrat-Light.ttf") format("truetype");
   font-weight: 300;
 }
 @font-face {
-  font-family: 'Montserrat';
-  src: local('Montserrat'), url('./assets/fonts/Montserrat/Montserrat-Regular.ttf') format('truetype');
+  font-family: "Montserrat";
+  src: local("Montserrat"),
+    url("./assets/fonts/Montserrat/Montserrat-Regular.ttf") format("truetype");
   font-weight: 400;
 }
 @font-face {
-  font-family: 'Montserrat';
-  src: local('Montserrat'), url('./assets/fonts/Montserrat/Montserrat-Medium.ttf') format('truetype');
+  font-family: "Montserrat";
+  src: local("Montserrat"),
+    url("./assets/fonts/Montserrat/Montserrat-Medium.ttf") format("truetype");
   font-weight: 500;
 }
 @font-face {
-  font-family: 'Montserrat';
-  src: local('Montserrat'), url('./assets/fonts/Montserrat/Montserrat-Bold.ttf') format('truetype');
+  font-family: "Montserrat";
+  src: local("Montserrat"),
+    url("./assets/fonts/Montserrat/Montserrat-Bold.ttf") format("truetype");
   font-weight: 600;
 }
 @font-face {
-  font-family: 'Montserrat';
-  src: local('Montserrat'), url('./assets/fonts/Montserrat/Montserrat-Black.ttf') format('truetype');
+  font-family: "Montserrat";
+  src: local("Montserrat"),
+    url("./assets/fonts/Montserrat/Montserrat-Black.ttf") format("truetype");
   font-weight: 700;
 }
 
 ///////////////////////// Roboto /////////////////////////
 @font-face {
-  font-family: 'Open Sans';
-  src: local('Open Sans'), url('./assets/fonts/Open_Sans/OpenSans-Light.ttf') format('truetype');
+  font-family: "Open Sans";
+  src: local("Open Sans"),
+    url("./assets/fonts/Open_Sans/OpenSans-Light.ttf") format("truetype");
   font-weight: 300;
 }
 @font-face {
-  font-family: 'Open Sans';
-  src: local('Open Sans'), url('./assets/fonts/Open_Sans/OpenSans-Regular.ttf') format('truetype');
+  font-family: "Open Sans";
+  src: local("Open Sans"),
+    url("./assets/fonts/Open_Sans/OpenSans-Regular.ttf") format("truetype");
   font-weight: 400;
 }
 @font-face {
-  font-family: 'Open Sans';
-  src: local('Open Sans'), url('./assets/fonts/Open_Sans/OpenSans-SemiBold.ttf') format('truetype');
+  font-family: "Open Sans";
+  src: local("Open Sans"),
+    url("./assets/fonts/Open_Sans/OpenSans-SemiBold.ttf") format("truetype");
   font-weight: 600;
 }
 @font-face {
-  font-family: 'Open Sans';
-  src: local('Open Sans'), url('./assets/fonts/Open_Sans/OpenSans-Bold.ttf') format('truetype');
+  font-family: "Open Sans";
+  src: local("Open Sans"),
+    url("./assets/fonts/Open_Sans/OpenSans-Bold.ttf") format("truetype");
   font-weight: 700;
 }
 @font-face {
-  font-family: 'Open Sans';
-  src: local('Open Sans'), url('./assets/fonts/Open_Sans/OpenSans-ExtraBold.ttf') format('truetype');
+  font-family: "Open Sans";
+  src: local("Open Sans"),
+    url("./assets/fonts/Open_Sans/OpenSans-ExtraBold.ttf") format("truetype");
   font-weight: 800;
 }
 
-::-webkit-scrollbar { 
+///////////////////////// Inter /////////////////////////
+@font-face {
+  font-family: "Inter";
+  src: local("Inter Thin"),
+    url("./assets/fonts/Inter/Inter-Thin.ttf") format("truetype");
+  font-weight: 100;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: "Inter";
+  src: local("Inter ExtraLight"),
+    url("./assets/fonts/Inter/Inter-ExtraLight.ttf") format("truetype");
+  font-weight: 200;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: "Inter";
+  src: local("Inter Light"),
+    url("./assets/fonts/Inter/Inter-Light.ttf") format("truetype");
+  font-weight: 300;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: "Inter";
+  src: local("Inter Regular"),
+    url("./assets/fonts/Inter/Inter-Regular.ttf") format("truetype");
+  font-weight: 400;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: "Inter";
+  src: local("Inter Medium"),
+    url("./assets/fonts/Inter/Inter-Medium.ttf") format("truetype");
+  font-weight: 500;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: "Inter";
+  src: local("Inter SemiBold"),
+    url("./assets/fonts/Inter/Inter-SemiBold.ttf") format("truetype");
+  font-weight: 600;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: "Inter";
+  src: local("Inter Bold"),
+    url("./assets/fonts/Inter/Inter-Bold.ttf") format("truetype");
+  font-weight: 700;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: "Inter";
+  src: local("Inter ExtraBold"),
+    url("./assets/fonts/Inter/Inter-ExtraBold.ttf") format("truetype");
+  font-weight: 800;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: "Inter";
+  src: local("Inter Black"),
+    url("./assets/fonts/Inter/Inter-Black.ttf") format("truetype");
+  font-weight: 900;
+  font-style: normal;
+}
+
+::-webkit-scrollbar {
   width: 8px;
   // height: 5px;
   // transition: 0.3s ease-in-out;
-  &:hover{
+  &:hover {
     width: 12px;
     // height: 100%;
   }
 }
 // ::-webkit-scrollbar-button {  background-color: #666; }
 // ::-webkit-scrollbar-track {  background-color: #999;}
-::-webkit-scrollbar-track-piece { background-color: #dfe3e7;}
-::-webkit-scrollbar-thumb { height: 50px; background-color: #0081CF; border-radius: 3px;}
-::-webkit-scrollbar-corner { background-color: #0081CF;}
-::-webkit-resizer { background-color: #0081CF;}
+::-webkit-scrollbar-track-piece {
+  background-color: #dfe3e7;
+}
+::-webkit-scrollbar-thumb {
+  height: 50px;
+  background-color: #0081cf;
+  border-radius: 3px;
+}
+::-webkit-scrollbar-corner {
+  background-color: #0081cf;
+}
+::-webkit-resizer {
+  background-color: #0081cf;
+}
 ::-webkit-scrollbar-thumb {
   background-color: #0164a1;
 }
-
 </style>
