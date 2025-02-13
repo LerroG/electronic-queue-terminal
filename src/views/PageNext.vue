13682f7f7b6cb96848240bf18c3ae0912ed2bb86
Ruslan <ssakoks@mail.ru>
2025-02-13 09:52:32 +0500

    fix numpadBg

--- src/views/PageNext.vue
+++ src/views/PageNext.vue
@@ -277,6 +277,9 @@
           width: 'auto',
           height: 'auto',
           zIndex: item.position.zIndex,
+          backgroundColor: `rgba(${hexToRgb(item.numpadBgColor)}, ${
+            item.numpadBgOpacity / 100
+          })`,
         }"
       >
         <input
@@ -1563,6 +1566,12 @@ export default {
             .catch((e) => console.log(e));
         }
       });
+    },
+    hexToRgb(hex) {
+      let r = parseInt(hex.substring(1, 3), 16);
+      let g = parseInt(hex.substring(3, 5), 16);
+      let b = parseInt(hex.substring(5, 7), 16);
+      return r + "," + g + "," + b;
     },
   },
   watch: {
